93e9fcf73eeadc2695d6804dda9bbe79de33f7de
Loc Diep <locthptctv@gmail.com>
2024-09-08 14:29:40 +0700

    Add shortcut to settings

--- dist/main.js
+++ dist/main.js
@@ -4,6 +4,7 @@ import ElectronStore from 'electron-store';
 let win;
 const isMac = process.platform === 'darwin';
 const isWin = process.platform === 'win32';
+const ctrlKey = isMac ? 'cmd' : 'ctrl';
 const store = new ElectronStore({ accessPropertiesByDotNotation: false });
 const defaultSettings = {
     'window.isMaximize': false,
@@ -21,19 +22,23 @@ const defaultSettings = {
     'workspace.panel.alignment': 'right',
     'workspace.statusBar.visible': true,
     'workspace.primarySideBarPosition': 'left',
+    'shortcut.togglePrimarySideBar': 'CmdOrCtrl+B',
+    'shortcut.toggleSecondarySideBar': 'Alt+CmdOrCtrl+B',
+    'shortcut.togglePanel': 'CommandOrControl+J',
+    'shortcut.fullscreen': 'Ctrl+Cmd+F'
 };
 function createWindow() {
     win = new BrowserWindow({
         frame: false,
         center: defaultSettings['window.isCenter'],
-        x: store.get('window.x', defaultSettings['window.x']),
-        y: store.get('window.y', defaultSettings['window.y']),
+        x: getSetting('window.x'),
+        y: getSetting('window.y'),
         titleBarStyle: isMac ? 'hidden' : 'default',
         trafficLightPosition: isMac ? { x: 10, y: 10 } : { x: 0, y: 0 },
-        width: store.get('window.width', defaultSettings['window.width']),
-        height: store.get('window.height', defaultSettings['window.height']),
-        minWidth: store.get('window.minWidth', defaultSettings['window.minWidth']),
-        minHeight: store.get('window.minHeight', defaultSettings['window.minHeight']),
+        width: getSetting('window.width'),
+        height: getSetting('window.height'),
+        minWidth: getSetting('window.minWidth'),
+        minHeight: getSetting('window.minHeight'),
         webPreferences: {
             preload: path.join(app.getAppPath(), 'dist', 'preload.js'),
             contextIsolation: true,
@@ -57,10 +62,10 @@ function createWindow() {
     }
     win.on('close', () => {
         const bound = win.getBounds();
-        store.set('window.x', bound.x);
-        store.set('window.y', bound.y);
-        store.set('window.width', bound.width);
-        store.set('window.height', bound.height);
+        setSetting('window.x', bound.x);
+        setSetting('window.y', bound.y);
+        setSetting('window.width', bound.width);
+        setSetting('window.height', bound.height);
     });
 }
 app.whenReady().then(() => {
@@ -69,17 +74,16 @@ app.whenReady().then(() => {
         if (BrowserWindow.getAllWindows().length === 0)
             createWindow();
     });
-    globalShortcut.register('ctrl+B', () => {
+    globalShortcut.register(getSetting('shortcut.togglePrimarySideBar'), () => {
         win.webContents.send('global-shortcut', 'shortcut', 'ctrl+B');
     });
-    globalShortcut.register('ctrl+shift+B', () => {
+    globalShortcut.register(getSetting('shortcut.toggleSecondarySideBar'), () => {
         win.webContents.send('global-shortcut', 'shortcut', 'ctrl+shift+B');
     });
-    globalShortcut.register('ctrl+J', () => {
+    globalShortcut.register(getSetting('shortcut.togglePanel'), () => {
         win.webContents.send('global-shortcut', 'shortcut', 'ctrl+J');
     });
-    // TODO: F11 doesn't work
-    globalShortcut.register('F11', () => {
+    globalShortcut.register(getSetting('shortcut.fullscreen'), () => {
         win.setFullScreen(!win.isFullScreen());
     });
 });
@@ -112,23 +116,32 @@ if (isWin) {
         }
     });
 }
-ipcMain.handle('getStoreValue', (event, key) => {
+function setSetting(key, value) {
+    store.set(key, value);
+}
+function getSetting(key) {
     return store.get(key, defaultSettings[key]);
+}
+ipcMain.handle('getStoreValue', (event, key) => {
+    return getSetting(key);
 });
 ipcMain.handle('setStoreValue', (event, key, value) => {
-    store.set(key, value);
+    setSetting(key, value);
 });
 const contextMenu = Menu.buildFromTemplate([
     { label: 'Activity Bar', id: 'activity-bar-item', type: 'checkbox',
         click: () => { win.webContents.send('toggle-activity-bar'); }
     },
-    { label: 'Primary Side Bar', id: 'primary-side-bar-item', type: 'checkbox', accelerator: 'ctrl+B',
+    { label: 'Primary Side Bar', id: 'primary-side-bar-item', type: 'checkbox',
+        accelerator: getSetting('shortcut.togglePrimarySideBar'),
         click: () => { win.webContents.send('toggle-primary-side-bar'); }
     },
-    { label: 'Secondary Side Bar', id: 'secondary-side-bar-item', type: 'checkbox', accelerator: 'ctrl+alt+B',
+    { label: 'Secondary Side Bar', id: 'secondary-side-bar-item', type: 'checkbox',
+        accelerator: getSetting('shortcut.toggleSecondarySideBar'),
         click: () => { win.webContents.send('toggle-secondary-side-bar'); }
     },
-    { label: 'Panel', id: 'panel-item', type: 'checkbox', accelerator: 'ctrl+J',
+    { label: 'Panel', id: 'panel-item', type: 'checkbox',
+        accelerator: getSetting('shortcut.togglePanel'),
         click: () => { win.webContents.send('toggle-panel'); }
     },
     { label: 'Status Bar', id: 'status-bar-item', type: 'checkbox',
@@ -145,7 +158,7 @@ const contextMenu = Menu.buildFromTemplate([
     { label: 'Right', type: 'radio', checked: true },
     { label: 'Full', type: 'radio', checked: false },
     { type: 'separator' },
-    { label: 'Full Screen', type: 'checkbox', checked: false, accelerator: 'F11' },
+    { label: 'Full Screen', type: 'checkbox', checked: false, accelerator: getSetting('shortcut.fullscreen'), },
 ]);
 ipcMain.on('set-checked', (event, id, checked) => {
     const menuItem = contextMenu.getMenuItemById(id);
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import ElectronStore from 'electron-store';
 let win: BrowserWindow;
 const isMac = process.platform === 'darwin';
 const isWin = process.platform === 'win32';
+const ctrlKey = isMac ? 'cmd' : 'ctrl';
 
 type DefaultSettings = {
   'window.isMaximize': boolean,
@@ -22,6 +23,10 @@ type DefaultSettings = {
   'workspace.panel.alignment': 'left' | 'center' | 'right' | 'full',
   'workspace.statusBar.visible': boolean,
   'workspace.primarySideBarPosition': 'left' | 'right',
+  'shortcut.togglePrimarySideBar': string,
+  'shortcut.toggleSecondarySideBar': string,
+  'shortcut.togglePanel': string,
+  'shortcut.fullscreen': string,
 };
 
 const store = new ElectronStore<DefaultSettings>({accessPropertiesByDotNotation: false});
@@ -42,20 +47,24 @@ const defaultSettings: DefaultSettings = {
   'workspace.panel.alignment': 'right',
   'workspace.statusBar.visible': true,
   'workspace.primarySideBarPosition': 'left',
+  'shortcut.togglePrimarySideBar': 'CmdOrCtrl+B',
+  'shortcut.toggleSecondarySideBar': 'Alt+CmdOrCtrl+B',
+  'shortcut.togglePanel': 'CommandOrControl+J',
+  'shortcut.fullscreen': 'Ctrl+Cmd+F'
 }
 
 function createWindow() {
   win = new BrowserWindow({
     frame: false,
     center: defaultSettings['window.isCenter'],
-    x: store.get('window.x', defaultSettings['window.x']),
-    y: store.get('window.y', defaultSettings['window.y']),
+    x: getSetting('window.x'),
+    y: getSetting('window.y'),
     titleBarStyle: isMac ? 'hidden' : 'default',
     trafficLightPosition: isMac ? { x: 10, y: 10 } : { x: 0, y: 0 },
-    width: store.get('window.width', defaultSettings['window.width']),
-    height: store.get('window.height', defaultSettings['window.height']),
-    minWidth: store.get('window.minWidth', defaultSettings['window.minWidth']),
-    minHeight: store.get('window.minHeight', defaultSettings['window.minHeight']),
+    width: getSetting('window.width'),
+    height: getSetting('window.height'),
+    minWidth: getSetting('window.minWidth'),
+    minHeight: getSetting('window.minHeight'),
     webPreferences: {
       preload: path.join(app.getAppPath(), 'dist', 'preload.js'),
       contextIsolation: true,
@@ -85,10 +94,10 @@ function createWindow() {
 
   win.on('close', () => {
     const bound = win.getBounds();
-    store.set('window.x', bound.x);
-    store.set('window.y', bound.y);
-    store.set('window.width', bound.width);
-    store.set('window.height', bound.height);
+    setSetting('window.x', bound.x);
+    setSetting('window.y', bound.y);
+    setSetting('window.width', bound.width);
+    setSetting('window.height', bound.height);
   });
 }
 
@@ -99,17 +108,16 @@ app.whenReady().then(() => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   });
 
-  globalShortcut.register('ctrl+B', () => {
+  globalShortcut.register(getSetting('shortcut.togglePrimarySideBar'), () => {
     win.webContents.send('global-shortcut', 'shortcut', 'ctrl+B');
   });
-  globalShortcut.register('ctrl+shift+B', () => {
+  globalShortcut.register(getSetting('shortcut.toggleSecondarySideBar'), () => {
     win.webContents.send('global-shortcut', 'shortcut', 'ctrl+shift+B');
   });
-  globalShortcut.register('ctrl+J', () => {
+  globalShortcut.register(getSetting('shortcut.togglePanel'), () => {
     win.webContents.send('global-shortcut', 'shortcut', 'ctrl+J');
   });
-  // TODO: F11 doesn't work
-  globalShortcut.register('F11', () => {
+  globalShortcut.register(getSetting('shortcut.fullscreen'), () => {
     win.setFullScreen(!win.isFullScreen());
   });
 });
@@ -148,25 +156,36 @@ if (isWin) {
   });
 }
 
-ipcMain.handle('getStoreValue', (event, key) => {
+function setSetting(key: string, value: any) {
+  store.set(key, value);
+}
+
+function getSetting(key: string): any {
   return store.get(key, defaultSettings[key as keyof typeof defaultSettings]);
+}
+
+ipcMain.handle('getStoreValue', (event, key) => {
+  return getSetting(key);
 });
 
 ipcMain.handle('setStoreValue', (event, key, value) => {
-  store.set(key, value);
+  setSetting(key, value);
 });
 
 const contextMenu = Menu.buildFromTemplate([
   { label: 'Activity Bar', id: 'activity-bar-item', type: 'checkbox',
     click: () => {win.webContents.send('toggle-activity-bar')}
   },
-  { label: 'Primary Side Bar', id: 'primary-side-bar-item', type: 'checkbox', accelerator: 'ctrl+B',
+  { label: 'Primary Side Bar', id: 'primary-side-bar-item', type: 'checkbox',
+    accelerator: getSetting('shortcut.togglePrimarySideBar'),
     click: () => {win.webContents.send('toggle-primary-side-bar')}
   },
-  { label: 'Secondary Side Bar', id: 'secondary-side-bar-item', type: 'checkbox', accelerator: 'ctrl+alt+B',
+  { label: 'Secondary Side Bar', id: 'secondary-side-bar-item', type: 'checkbox',
+    accelerator: getSetting('shortcut.toggleSecondarySideBar'),
     click: () => {win.webContents.send('toggle-secondary-side-bar')}
   },
-  { label: 'Panel', id: 'panel-item', type: 'checkbox', accelerator: 'ctrl+J',
+  { label: 'Panel', id: 'panel-item', type: 'checkbox',
+    accelerator: getSetting('shortcut.togglePanel'),
     click: () => {win.webContents.send('toggle-panel')}
   },
   { label: 'Status Bar', id: 'status-bar-item', type: 'checkbox',
@@ -183,7 +202,7 @@ const contextMenu = Menu.buildFromTemplate([
   { label: 'Right', type: 'radio', checked: true},
   { label: 'Full', type: 'radio', checked: false},
   { type: 'separator' },
-  { label: 'Full Screen', type: 'checkbox', checked: false, accelerator: 'F11'},
+  { label: 'Full Screen', type: 'checkbox', checked: false, accelerator: getSetting('shortcut.fullscreen'),},
 ]);
 
 ipcMain.on('set-checked', (event, id, checked) => {
